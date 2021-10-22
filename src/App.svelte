a2dc456a887007689abd4332a181713491d20c67
Jonathan Piard <jhon.piard@toxiostek.com>
2021-10-22 19:26:12 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	const greeting = 'Hello Svelte'
+</script>
+
+<h1>{greeting}</h1>
+
+
+<style>
+	h1 {
+	color: orangered;
+	text-align: center;
+	font-family: 'Lucida Sans', Verdana, sans-serif;
+	}
+</style>
